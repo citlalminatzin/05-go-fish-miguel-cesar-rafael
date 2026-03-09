d8af156405575f2107968cdc09ed782585163b6c
rflsg <softsailors84@ciencias.unam.mx>
2026-03-09 16:44:35 -0600

    feat: read_data ahora lee archivos csv

--- data.py
+++ data.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
+
 def read_data(path="data/pescados.csv"):
-    """Lee los datos de un csv y te los devuelve"""
-    ...
+    """Lee los datos del csv y los devuelve en una tupla de dos listas""""
+    long = []
+    peso = []
+    with open(path, newline="") as csvfile:
+        spamreader = csv.reader(csvfile)
+        next(spamreader, None)
+        for row in spamreader:
+            long.append(float(row[0]))
+            peso.append(float(row[1]))
+    return long, peso
