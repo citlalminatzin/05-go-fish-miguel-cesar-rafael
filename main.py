cedc16643a3539a8a87b6706c381e46808645c08
rflsg <softsailors84@ciencias.unam.mx>
2026-03-11 16:12:54 -0600

    feat: plot_data grafica los datos y los guarda en media

--- main.py
+++ main.py
@@ -2,6 +2,27 @@
 
 import matplotlib.pyplot as plt
 from models import calc_error, modelo_geom, modelo_circ
+from data import read_data
+
+
+def plot_data():
+    data = read_data()
+
+    long = data[0]
+    peso = data[1]
+
+    vol = []
+    for elem in long:
+        vol.append((elem * elem * elem) / 1000)
+
+    fig, ax = plt.subplots()
+    ax.plot(vol, peso, "o")
+    ax.set_title("Datos de los Peces")
+    ax.set_xlabel("Volumen en $1000cm²$ por unidad")
+    ax.set_ylabel("Peso en kg")
+    ax.grid()
+    plt.savefig("media/data.png")
+    plt.close()
 
 
 def make_plot():
@@ -10,15 +31,12 @@ def make_plot():
     Si repites mucho tu código para
     graficar puedes guardarlo en una función
     """
-    ... # Esto significa implementación pendiente, lo puedes eliminar
+    ...  # Esto significa implementación pendiente, lo puedes eliminar
+
 
 def main():
-    """
-    (Si no modificas esta cadena de texto lloro)
-    Aquí va el código, recuerda reutilizar el 
-    código que ya escribiste en otros archivos
-    """
-    ... # Esto significa implementación pendiente, lo puedes eliminar
+    plot_data()
+
 
 if __name__ == "__main__":
     main()
