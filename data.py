14229c89c81a7ce3143fbd57a8dc59ff6c9bc0eb
rflsg <softsailors84@ciencias.unam.mx>
2026-03-11 18:23:59 -0600

    feat: funcion de regresion lineal en data.py

--- data.py
+++ data.py
@@ -13,4 +13,24 @@ def read_data(path="data/pescados.csv"):
         for row in spamreader:
             long.append(float(row[0]))
             peso.append(float(row[1]))
-    return long, peso
+    return [long, peso]
+
+
+# De https://github.com/LeninPA/tm1-2026-2/tree/main/01-regresion
+def rls(x: list[float], y: list) -> list[float]:
+    n = len(x)
+
+    xy = [xi * yi for xi, yi in zip(x, y)]
+    x2 = [xi**2 for xi in x]
+
+    suma_x = sum(x)
+    avg_x = suma_x / n
+    suma_y = sum(y)
+    avg_y = suma_y / n
+    suma_xy = sum(xy)
+    suma_x2 = sum(x2)
+
+    alfa = (suma_xy - avg_y * suma_x) / (suma_x2 - avg_x * suma_x)
+    beta = avg_y - alfa * avg_x
+
+    return [alfa, beta]
