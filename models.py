008a4ed5231fa2a783dce260bbb1c48574310626
rflsg <softsailors84@ciencias.unam.mx>
2026-03-11 19:26:47 -0600

    feat: modelo geometrico implementado

--- models.py
+++ models.py
@@ -2,7 +2,7 @@
 """
 models.py
 
-(Por favor modifica o elimina este comentario) 
+(Por favor modifica o elimina este comentario)
 Es recomendable que escribas unas cuantas líneas
 explicando el propósito de cada código. Te propongo
 que utilices este archivo para que escribas las
@@ -10,33 +10,36 @@ funciones principales que vayas a reutilizar en
 tus otras prácticas
 """
 
-def modelo_geom(longitudes: list[float]) -> list[float]:
+
+def modelo_geom(longitudes: list[float], k: float) -> list[float]:
     """
-    longitudes: list[float] ¿Qué significa longitudes? 
-    (Por favor elimina la pregunta y reemplazala con su respuesta)
-    ...
+    longitudes: list[float] lista de las lonigutudes de los peces
+    k: float, pendiente aproximada
     """
-    ... # Puedes eliminar esta línea
+    return [k * (long**3) for long in longitudes]
+
 
 def modelo_circ(longitudes: list[float]) -> list[float]:
     """
-    longitudes: list[float] ¿Qué significa longitudes? 
+    longitudes: list[float] ¿Qué significa longitudes?
     (Por favor elimina la pregunta y reemplazala con su respuesta)
     ...
     """
-    ... # Puedes eliminar esta línea
+    ...  # Puedes eliminar esta línea
 
-def pearson(x:list[float], y: list[float]):
+
+def pearson(x: list[float], y: list[float]):
     """Calcula el coeficiente de pearson"""
     ...
 
-def calc_error(pred:list[float], truth: list[float]):
+
+def calc_error(pred: list[float], truth: list[float]):
     """Calcula el error entre una predicción y la verdad del dataset"""
 
-def main():
-    ... # Puedes eliminar esta línea
+
+def main(): ...  # Puedes eliminar esta línea
+
 
 if __name__ == "__main__":
     # Si necesitas hacer pruebas de tu función las puedes escribir acá
     main()
-
